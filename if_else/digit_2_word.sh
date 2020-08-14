a0fead9169023ef59f838dbd38d283e110fdfbee
tushar.sable <tushar.sabe@gmail.com>
2020-08-14 17:50:24 +0530

    read single digit number and write the number in wordmber in word

---
+++ if_else/digit_2_word.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+read -p "Enter single digit number" number
+if (($number == 1))
+then
+	echo "One"
+
+elif (($number == 2))
+then
+	echo "Two"
+
+elif (($number == 3))
+then
+	echo "Three"
+
+elif (($number == 4))
+then
+	echo "Four"
+
+elif (($number == 5))
+then
+	echo "five"
+
+elif (($number == 6))
+then
+	echo "Six"
+
+elif (($number == 7))
+then
+	echo "Seven"
+
+elif (($number == 8))
+then
+	echo "Eight"
+
+elif (($number == 9))
+then
+	echo "Nine"
+
+fi
