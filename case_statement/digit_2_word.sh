aeec6e0c99ba5c84718c26942883ce5bf2e4c228
tushar.sable <tushar.sabe@gmail.com>
2020-08-14 19:06:22 +0530

    using case statement convert a single digit number to word

---
+++ case_statement/digit_2_word.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+#read number from user
+read -p "Enter single digit number" number
+
+#check for number in each case and print accordingly
+case $number in
+	
+	1)
+	echo "One"
+	;;
+	
+	2)
+	echo "Two"
+	;;
+
+	3)
+	echo "Three"
+	;;
+
+	4)
+	echo "Four"
+	;;
+
+	5)
+	echo "five"
+	;;
+
+	6)
+	echo "Six"
+	;;
+
+	7)
+	echo "Seven"
+	;;
+
+	8)
+	echo "Eight"
+	;;
+
+	9)
+	echo "Nine"
+	;;
+
+esac
