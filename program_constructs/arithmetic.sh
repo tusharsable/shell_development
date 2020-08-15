f8a8a8f52b4ff2c67306167b57b7756d392fdb3f
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 20:58:46 +0530

    evaluate expression 1

--- program_constructs/arithmetic.sh
+++ program_constructs/arithmetic.sh
@@ -7,5 +7,9 @@ echo "Welcome to arithmetic program for computation"
 
 #get 3 numbers to evaluate mathematical expressions
 read -p "enter 3 numbers" a b c
-echo $a $b $c
 
+
+#evaluate math expressions and store in respective variables
+math_1=$((a+b*c))
+
+echo $math_1
