3814c032fb319ce415ba196cf50e9179423ae23d
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 17:01:05 +0530

    calculate monthly wage of an employee

--- employee_wage/employee_wage_calculator.sh
+++ employee_wage/employee_wage_calculator.sh
@@ -3,9 +3,10 @@
 #Display title to user
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
-#Initializing Wage per hour and  full Day hour
+#Initializing day and wage per hour and total wage
 wage_per_hour=20
-
+day=1
+wage_total=0
 
 #get if employee is working part time and not full time
 is_part_time=$(($RANDOM%2))
@@ -20,11 +21,25 @@ case $is_part_time in
 	1)	#if is_part_time equals 0 means full time employee
 		hours_per_day=4
 		;;
+
 esac
 
-#Generate Attendance of employee 0 if absent and 1 for present
-attendance=$(($RANDOM%2))
+#for every day calculate daily wage and find total wage
+while (( $day <= 20 ))
+do
 
-#Get day wage of Employee
-employee_day_wage=$(($attendance*$wage_per_hour*$hours_per_day))
 
+	#Generate Attendance of employee 0 if absent and 1 for present
+	attendance=$(($RANDOM%2))
+
+	#Get day wage of Employee
+	employee_day_wage=$(($attendance*$wage_per_hour*$hours_per_day))
+	
+	#Get total wage till date in month
+	wage_total=$(($wage_total+$employee_day_wage))
+
+	#increment day
+	day=$(($day+1))
+
+done
+echo "monthly wage is : "$wage_total
