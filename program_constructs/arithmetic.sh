aee272d396ca90c8d9dfca6143192bd5e736a9b1
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 21:22:28 +0530

    stored the values from dictionary to an array

--- program_constructs/arithmetic.sh
+++ program_constructs/arithmetic.sh
@@ -4,7 +4,6 @@
 echo "Welcome to arithmetic program for computation"
 
 
-
 #get 3 numbers to evaluate mathematical expressions
 read -p "enter 3 numbers" a b c
 
@@ -16,4 +15,15 @@ math[1]=$((a+b*c))
 math[2]=$((a%b+c))
 math[3]=$((c+a/b))
 math[4]=$((a*b+c))
-echo ${math[@]}
+count1=0
+count2=0
+
+for x in ${math[@]}
+do
+
+	
+	#assign new random number to array
+	arr[((count1++))]=$x
+done
+
+echo ${arr[@]}
