ee0e8de6db1eb48cf8dd877801e1351d4dde53d4
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 14:53:06 +0530

    Get attendance of user

--- employee_wage/employee_wage_calculator.sh
+++ employee_wage/employee_wage_calculator.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 
+#Display title to user
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+#Generate Attendance of employee 0 if absent and 1 for present
+attendance=$(($RANDOM%2))
