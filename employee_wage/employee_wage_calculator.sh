4ea91b9052ef50d7b93157aa3cd3be7a956e1960
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 16:43:32 +0530

    assigned appropriate working hours for full adn part time employees

--- employee_wage/employee_wage_calculator.sh
+++ employee_wage/employee_wage_calculator.sh
@@ -5,10 +5,22 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 
 #Initializing Wage per hour and  full Day hour
 wage_per_hour=20
-hours_per_day=8
+
 
 #get if employee is working part time and not full time
 is_part_time=$(($RANDOM%2))
+
+# check if full time or part time employee and assign working hours accordingly
+case $is_part_time in
+
+	0)	#if is_part_time equals 0 means full time employee
+		hours_per_day=8
+		;;
+
+	1)	#if is_part_time equals 0 means full time employee
+		hours_per_day=4
+		;;
+esac
 
 #Generate Attendance of employee 0 if absent and 1 for present
 attendance=$(($RANDOM%2))
