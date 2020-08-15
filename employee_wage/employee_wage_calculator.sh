d958063658a0c1d821f32eb73713882acc26fd51
tushar.sable <tushar.sabe@gmail.com>
2020-08-15 15:09:09 +0530

    calculate day wage of employee

--- employee_wage/employee_wage_calculator.sh
+++ employee_wage/employee_wage_calculator.sh
@@ -3,5 +3,14 @@
 #Display title to user
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+#Initializing Wage per hour and  full Day hour
+wage_per_hour=20
+hours_per_day=8
+
+
 #Generate Attendance of employee 0 if absent and 1 for present
 attendance=$(($RANDOM%2))
+
+#Get day wage of Employee
+employee_day_wage=$(($attendance*$wage_per_hour*$hours_per_day))
+
