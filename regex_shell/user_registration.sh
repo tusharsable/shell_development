6881fbe79e34f053bf89f7c7e6a207cdf59181b9
tushar.sable <tushar.sabe@gmail.com>
2020-08-23 12:29:19 +0530

    Check user E-mail id

--- regex_shell/user_registration.sh
+++ regex_shell/user_registration.sh
@@ -6,6 +6,32 @@ shopt -s extglob
 #Program to register a user
 
 
+#Pattern to check if first letter capital and name length greater than 3
+pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+#Read User First Name
+read -p "Enter Your First Name" first_name
+
+#Check First Name
+if [[ $first_name =~ $pat ]]
+then
+        echo Your First name is $first_name
+else
+        echo Please Enter your name with first letter capital
+fi
+
+
+#Read User Last Name
+read -p "Enter Your Last Name" last_name
+
+#Check Last Name
+if [[ $last_name =~ $pat ]]
+then
+        echo Your Last name is $last_name
+else
+        echo Please Enter correct name with first capital letter
+fi
+
 #read Email from user
 read -p "Enter your E-mail Id" email
 
