40781e698ed120e49a9a5a44f9db91437fd15e5f
tushar.sable <tushar.sabe@gmail.com>
2020-08-23 12:27:41 +0530

    Check user E-mail id

--- regex_shell/user_registration.sh
+++ regex_shell/user_registration.sh
@@ -6,29 +6,17 @@ shopt -s extglob
 #Program to register a user
 
 
-#Pattern to check if first letter capital and name length greater than 3
-pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+#read Email from user
+read -p "Enter your E-mail Id" email
 
-#Read User First Name
-read -p "Enter Your First Name" first_name
+#Pattern for email
+pat_email="^[a-z]{1,}[a-z0-9\.\_\-]*[a-z0-9](\@[a-z]{3,})\.([a-z]{2,}|[a-z]{2,}\.[a-z]{2,})$"
 
-#Check First Name
-if [[ $first_name =~ $pat ]]
+
+#Check email
+if [[ $email =~ $pat_email ]]
 then
-	echo Your First name is $first_name
+	echo Your E-mail Id  is $email
 else
-	echo Please Enter your name with first letter capital
+	echo Please Enter correct E-mail Id
 fi
-
-
-#Read User Last Name
-read -p "Enter Your Last Name" last_name
-
-#Check Last Name
-if [[ $last_name =~ $pat ]]
-then
-	echo Your Last name is $last_name
-else
-	echo Please Enter correct name with first capital letter
-fi
-
