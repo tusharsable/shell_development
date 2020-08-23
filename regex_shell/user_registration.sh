c0a774dac6c4abb03c4d87459f966ecdce6ecea8
tushar.sable <tushar.sabe@gmail.com>
2020-08-23 10:26:29 +0530

    check first name

--- regex_shell/user_registration.sh
+++ regex_shell/user_registration.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -x
 
+#turn on external globbing
+shopt -s extglob
 
 #Program to register a user
+read -p "Enter Your First Name" first_name
+
+#Pattern to check if first letter capital and name length greater than 3
+pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+
+if [[ $first_name =~ $pat ]]
+then
+	echo correct username
+else
+	echo wrong username
+fi
+
+
