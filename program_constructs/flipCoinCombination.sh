d1872bf93fc19d64bef52776e7cc2ae84606dcdb
tushar.sable <tushar.sabe@gmail.com>
2020-08-28 17:35:37 +0530

    new program for flip combination

---
+++ program_constructs/flipCoinCombination.sh
@@ -0,0 +1,12 @@
+#Program to simulate flipping of a coin
+echo "Welcome to coin flipping simulator"
+
+#initialize count to 0
+count_heads=0
+count_tails=0
+
+#get random number between 0,1 and check if its 1
+flip=$(($RANDOM%2))
+echo $flip
+
+
