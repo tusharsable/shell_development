c7114b37859e77b486bfa8237004f710178e1bc2
tushar.sable <tushar.sabe@gmail.com>
2020-08-28 17:56:40 +0530

    Flip singlet and print percent

--- program_constructs/flipCoinCombination.sh
+++ program_constructs/flipCoinCombination.sh
@@ -5,8 +5,24 @@ echo "Welcome to coin flipping simulator"
 count_heads=0
 count_tails=0
 
+
+for((count=0;count<20;count++))
+do
+
 #get random number between 0,1 and check if its 1
-flip=$(($RANDOM%2))
-echo $flip
+if(($(($RANDOM%2)) == 1 ))
+then
+	#if its 1 then incrase count of heads
+	count_heads=$(($count_heads + 1))
+else
+	#else increase tails count by 1
+	count_tails=$(($count_tails + 1))
+fi
 
 
+done
+
+echo "Heads percentage : " $(awk '{print ($1*100/($1+$2))}' <<< "${count_heads} ${count_tails}")
+
+
+echo "Tails percentage : " $(awk '{print ($1*100/($1+$2))}' <<< "${count_tails} ${count_heads}")
